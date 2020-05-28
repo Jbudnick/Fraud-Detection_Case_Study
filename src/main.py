07756dea3ab956003d2fc1a7826f894a38ea9183
Sam Silver <sasi1851@colorado.edu>
2020-05-28 19:26:09 -0400

    updated main notes

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 #Try to get to a model today
+    #
 #dedicate one person to start flask app
 #Divide and Conquer
 
@@ -12,15 +13,31 @@
 # Jacob - columns has_analytics to payout type
 # Joe - previous payouts to end
     
-    # len(df['previous_payouts'][1]) == 0
-    # no previous payouts, suspicious
 
-    # user age = 0 or <100
-    #some outliers with 800+
+#TODO
+    #DATABASE
+        '''
+        Set up a Postgres or MongoDB database that will store each example that the script runs on. 
+        You should create a database schema that reflects the form of the raw example data and add a 
+        column for the predicted probability of fraud.
 
-    # user type  
-    # 70% fraud events user type =1
-    # 25% all events user type =1
+        predict.py 
+            loads model, loads example(example.json), runs example through feature engineering pipeline,
+            adds columns from original training data to match the structure,gets probability of fraud,
+            makes prediction based on a specified threshold
+        
+        for each example calculate the probability of fraud and add to database along with event info
+        
+        '''
+    #WEB APP
+    '''
+        Hello world flask app tutorial?
+        
+        Set up a route POST /score and have it execute the logic in your prediction script. 
+        You should import the script as a module and call functions defined therein. (predict.py)
+    There are two things we'll do to make this all more efficient:
 
-
-{'classifier__n_estimators': 368, 'classifier__min_samples_split': 2, 'classifier__min_samples_leaf': 1, 'classifier__max_features': 'sqrt', 'classifier__max_depth': None, 'classifier__bootstrap': False}
+We only want to unpickle the model once
+We only want to connect to the database once.
+    '''
+    
